4ef17314086f40f5e62ca36fe596f20f143c6e48
Ark Raj <121331379+arkraj@users.noreply.github.com>
2023-04-20 14:03:33 +0530

    Update connection.php

--- shared/connection.php
+++ shared/connection.php
@@ -1,6 +1,13 @@
 <?php
 
-$conn= new mysqli("localhost","ark","abc123","ecommerce");
+$host = 'localhost';
+$username = 'ark';
+$password = 'abc123';
+$dbname = 'ecommerce';
+
+//connect to the database 
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
 if($conn->connect_error)
 {
     echo " Error in connecting database";
