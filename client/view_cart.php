ea330811ff25211595e5bccf16429cfe1004a49e
Ark Raj <121331379+arkraj@users.noreply.github.com>
2023-04-20 14:15:58 +0530

    Update view_cart.php

--- client/view_cart.php
+++ client/view_cart.php
@@ -1,10 +1,5 @@
 <?php
-$conn= new mysqli("localhost","ark","abc123","ecommerce");
-if($conn->connect_error)
-{
-    echo " Error in connecting database";
-    die;
-}
+include('../shared/connection.php');
 session_start();
 // Check if the user is logged in
 if (!isset($_SESSION['email'])) {
